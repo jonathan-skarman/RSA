9ed12788dea688d98b1a8ee1ab7b744f1da06523
jonathan skarman <kaboomgf@gmail.com>
2025-02-21 12:16:04 +0100

    made work

--- lib/functions.rb
+++ lib/functions.rb
@@ -5,12 +5,13 @@ end
 
 def gcd(a, b)
   while b != 0
+    a, b = b, a % b
   end
   a
 end
 
 def mod_inverse(e, phi_n)
-  d = 0
+  d = 1
   while (e*d) % phi_n != 1
     d += 1
   end
@@ -19,4 +20,47 @@ end
 
 def mod_exp(num, exponent, modulo_base)
   num % modulo_base == 0 ? 0 : num ** exponent % modulo_base
+end
+
+#första subbarrayen är public key, andra är private key
+def key_generator(e, p, q)
+  if !relative_prime((p-1)*(q-1), e)
+    raise ArgumentError.new("e must be relative prime to (p-1)*(q-1)")
+  end
+
+  n = p*q
+  phi_n = (p-1)*(q-1)
+  d = mod_inverse(e, phi_n)
+
+  return[[e, n], [d, n]]
+end
+
+def encode(message, e, n)
+  arr = []
+  message.bytes.each do |byte|
+    arr << mod_exp(byte, e, n).to_i
+  end
+
+  arr
+end
+
+def decode(y, d, n)
+  arr = []
+  y.each do |byte|
+    arr << mod_exp(byte, d, n).to_i
+  end
+
+  arr.pack("C*")
+end
+
+def hack_key(e, n)
+  p = 2
+  while n % p != 0
+    p += 1
+  end
+  q = n / p
+
+  d = mod_inverse(e, ((p-1)*(q-1)))
+
+  return [d, n]
 end
---
+++ lib/runner.rb
@@ -0,0 +1,25 @@
+require_relative "functions"
+
+e = 7
+p = 61
+q = 53
+message = "Hello, World!"
+
+
+
+public_key, private_key = key_generator(e, p, q)
+encoded_message = encode(message, public_key[0], public_key[1])
+hacked_key = hack_key(public_key[0], public_key[1])
+decoded_message = decode(encoded_message, private_key[0], private_key[1])
+hacked_message = decode(encoded_message, hacked_key[0], hacked_key[1])
+
+
+
+puts "Public key: #{public_key}"
+puts "Private key: #{private_key}"
+
+puts "Encoded message: #{encoded_message}"
+puts "Decoded message: #{decoded_message}"
+
+puts "Hacked key: #{hacked_key}"
+puts "Hacked message: #{hacked_message}"
